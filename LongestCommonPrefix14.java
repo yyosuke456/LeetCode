1d6c4792f865a629c7b2321805e0d2a9364c74b7
Yosuke Yamaguchi <yonosho456@gmail.com>
2021-02-01 23:43:15 +0900

    return empty string if size is zero. End if run times as min str length

--- LongestCommonPrefix14.java
+++ LongestCommonPrefix14.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -6,7 +7,15 @@ class Solution {
   public String longestCommonPrefix(String[] strs) {
     String result = "";
     List<String> strList = Arrays.asList(strs);
-    for (int i = 0; i < strList.size(); i++) {
+    if (strList.size() == 0) {
+      return result;
+    }
+    if (strList.get(0).length() == 0) {
+      return result;
+    }
+
+    int minLength = strList.stream().min((l, r) -> l.length() - r.length()).orElse("").length();
+    for (int i = 0; i < minLength; i++) {
       int index = i;
       String firstStr = strList.get(0).substring(index, index + 1);
       List<String> filteredList = strList.stream().filter(s -> s.substring(index, index + 1).equals(firstStr))
